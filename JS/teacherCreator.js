85db59efb8e1e54bf6c90e39b028ced73b0ae2ed
Jomfruland <ingvild@macrell.no>
2022-03-31 16:55:53 +0200

    list out the teachers with js functions

--- JS/teacherCreator.js
+++ JS/teacherCreator.js
@@ -1,35 +1,52 @@
-// Fetch Api
-let url = "http://hp-api.herokuapp.com/api/characters";
-function fetchData(houseName) {
-  fetch(url)
-    .then((response) => {
+fetch("http://hp-api.herokuapp.com/api/characters")
+  .then((response) => {
+    console.log(response);
+    if (response.ok) {
       return response.json();
-    })
-    .then((data) => {
-      renderData(data, houseName);
-    });
-}
-let hogwartsStaff;
-function renderData(data, houseName) {
-  hogwartsStaff = data.filter(function (data) {
-    return data.house == houseName;
+    }
+    throw "feilmelding";
+  })
+  .then((data) => {
+    displayTeacher(renderData(data));
+  })
+  .catch((err) => console.log("first", err));
+
+let staffMembers;
+
+//filtrerer staffmembers
+
+function renderData(data) {
+  staffMembers = data.filter(function (data) {
+    return data.hogwartsStaff == true;
   });
-  console.log(hogwartsStaff);
+  console.log(staffMembers);
+  return staffMembers;
 }
 
-//hogwartsStaff;
-//"hogwartsStaff":true
-/*
-document.querySelector(".gryffindor-btn").onclick = () => {
-  fetchData("Gryffindor");
+// Display all staffmembers with name and image
+
+const teacherList = document.querySelector(".teacher-list");
+
+const displayTeacher = (hogwartsStaffList) => {
+  console.log(hogwartsStaffList);
+  hogwartsStaffList.forEach((staffMember) => {
+    let placeholder = staffMember.image;
+    if (staffMember.image === "") {
+      placeholder = "./images/defaultimage.png";
+    }
+    teacherList.innerHTML += `<li class="">
+        <h2>${staffMember.name}</h2>
+        <img src="${placeholder}" class=""/>
+        </li>
+        `;
+  });
+
+  // const htmlString = staffMembers
+  //   .map((hogwartsStaff) => {
+  //     let placeholder = hogwartsStaff.image;
+  //     if (staffMembers.image === "") {
+  //       placeholder = "./images/defaultimage.png";
+  //     }return `<li class="character">
+  //       <h2>${hogwartsStaff.name}</h2>
+  // teacherList.innerHTML = htmlString;
 };
-document.querySelector(".slytherin-btn").onclick = () => {
-  fetchData("Slytherin");
-};
-document.querySelector(".hufflepuff-btn").onclick = () => {
-  fetchData("Hufflepuff");
-};
-document.querySelector(".ravenclaw-btn").onclick = () => {
-  fetchData("Ravenclaw");
-};
-*/
